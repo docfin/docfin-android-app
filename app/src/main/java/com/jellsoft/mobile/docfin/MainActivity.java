c54052f785e070c05084fb6bc44b5c158c2bb8a0
atulanand <atulanand77@gmail.com>
2016-07-31 20:56:57 -0400

    initial commits

--- app/src/main/java/com/jellsoft/mobile/docfin/MainActivity.java
+++ app/src/main/java/com/jellsoft/mobile/docfin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jellsoft.mobile.docfin;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +13,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = (EditText) findViewById(R.id.firstName);
-        editText.requestFocus();
+
+        findViewById(R.id.insuranceId).clearFocus();
+        findViewById(R.id.firstName).requestFocus();
     }
 
     public void registerUser(View view) {
         //view is the button.
-        System.out.println("Register User");
+        validateUserRegistrationFields();
+    }
+
+    private void validateUserRegistrationFields()
+    {
+        validateEditText((EditText)findViewById(R.id.firstName));
+        validateEditText((EditText)findViewById(R.id.lastName));
+        validateEditText((EditText)findViewById(R.id.emailId));
+        validateEditText((EditText)findViewById(R.id.insuranceId));
+    }
+
+    private void validateEditText(EditText editText)
+    {
+        if(editText.getText().toString().trim().isEmpty())
+        {
+            switch (editText.getId())
+            {
+                case R.id.firstName:
+                    editText.setError(getString(R.string.user_firstName_error));
+                    break;
+                case R.id.lastName:
+                    editText.setError(getString(R.string.user_lastName_error));
+                    break;
+                case R.id.emailId:
+                    editText.setError(getString(R.string.user_email_error));
+                    break;
+                case R.id.insuranceId:
+                    editText.setError(getString(R.string.user_insurance_error));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
