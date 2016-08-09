80b3d30cdaa97bf4248683f5c2666da186df6404
atulanand <atulanand77@gmail.com>
2016-08-08 23:13:30 -0400

    added a placeholder for search activity and the toolbar template

---
+++ app/src/main/java/com/jellsoft/mobile/docfin/DoctorSearchActivity.java
@@ -0,0 +1,13 @@
+package com.jellsoft.mobile.docfin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_search);
+    }
+}
