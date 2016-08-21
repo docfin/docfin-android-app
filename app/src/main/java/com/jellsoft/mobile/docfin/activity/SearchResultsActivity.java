7489a4798381b02a7c1b3c6480c9a836cae266cc
atulanand <atulanand77@gmail.com>
2016-08-21 18:06:35 -0400

    started search results activity

---
+++ app/src/main/java/com/jellsoft/mobile/docfin/activity/SearchResultsActivity.java
@@ -0,0 +1,14 @@
+package com.jellsoft.mobile.docfin.activity;
+
+import android.os.Bundle;
+
+import com.jellsoft.mobile.docfin.R;
+
+public class SearchResultsActivity extends BaseDocfinActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
